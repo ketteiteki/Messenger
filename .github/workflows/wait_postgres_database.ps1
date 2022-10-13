fa0ad8dcd2123751b29ebd43ce954841e5558cff
quetly3301 <petro.kolosov@icloud.com>
2022-10-13 19:53:03 +0200

    workflow added

---
+++ .github/workflows/wait_postgres_database.ps1
@@ -0,0 +1,15 @@
+$ContainerName = 'messenger-pgsql-db'
+$ContainerLogPatternForDatabaseReady = "SQL Server is now ready for client connections. This is an informational message; no user action is required."
+        
+for(;;) {
+    $isDatabaseReady = 
+    docker logs --tail 100 $ContainerName | Select-String -Pattern $ContainerLogPatternForDatabaseReady -SimpleMatch -Quiet
+        
+    if ($isDatabaseReady -eq $true) {
+        Write-Output "`n`nDatabase running inside container ""$ContainerName"" is ready to accept incoming connections"
+        break;
+    }
+    
+    Write-Output "Database is not ready."
+    Start-Sleep -Seconds 1
+}
