ec550f9ccb2c3287a0f6fc0ff758cc2bd4d5ccb5
Ketteiteki <kudryashov.kd@gmail.com>
2023-05-25 01:00:40 +0400

    add signalr filter

--- Messenger.BusinessLogic/Hubs/ChatHub.cs
+++ Messenger.BusinessLogic/Hubs/ChatHub.cs
@@ -1,18 +1,59 @@
+using Messenger.Domain.Constants;
+using Messenger.Persistence;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.SignalR;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Caching.Memory;
 
 namespace Messenger.BusinessLogic.Hubs;
 
 [Authorize]
 public class ChatHub : Hub<IChatHub>
 {
-    public Task JoinChat(Guid chatId)
+    private readonly DatabaseContext _context;
+    private readonly MemoryCache _memoryCache;
+    private readonly MemoryCacheEntryOptions _memoryCacheEntryOptions;
+    
+    public ChatHub(DatabaseContext context, MemoryCache memoryCache)
     {
-        return Groups.AddToGroupAsync(Context.ConnectionId, chatId.ToString());
+        _context = context;
+        _memoryCache = memoryCache;
+        _memoryCacheEntryOptions = new MemoryCacheEntryOptions()
+            .SetSlidingExpiration(TimeSpan.FromSeconds(5))
+            .SetAbsoluteExpiration(TimeSpan.FromSeconds(10));
+    }
+
+    public async Task JoinChat(Guid chatId)
+    {
+        var userId = Context.User?.Claims.First(x => x.Type == ClaimConstants.Id).Value;
+
+        var isGettingEntriesSuccessful = _memoryCache.TryGetValue(userId, out List<UserChatEntry> userChatEntries);
+        
+        if (isGettingEntriesSuccessful)
+        {
+            var isChatInEntries = userChatEntries.Any(x => x.ChatId == chatId);
+            
+            if (isChatInEntries)
+            {
+                await Groups.AddToGroupAsync(Context.ConnectionId, chatId.ToString());
+                return;
+            }
+        }
+        
+        var userChats = await _context.ChatUsers
+            .Where(x => x.UserId == new Guid(userId))
+            .Select(x => new UserChatEntry(x.UserId, x.ChatId))
+            .ToListAsync();
+
+        _memoryCache.Set(userId, userChats, _memoryCacheEntryOptions);
+        
+        await Groups.AddToGroupAsync(Context.ConnectionId, chatId.ToString());
     }
     
-    public Task LeaveChat(Guid chatId)
+    public async Task LeaveChat(Guid chatId)
     {
-        return Groups.RemoveFromGroupAsync(Context.ConnectionId, chatId.ToString());
+        await Groups.RemoveFromGroupAsync(Context.ConnectionId, chatId.ToString());
     }
+
+    private record UserChatEntry(Guid UserId, Guid ChatId);
 }
